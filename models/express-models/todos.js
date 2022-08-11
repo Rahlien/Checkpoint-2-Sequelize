f1853206a17e42031fa2496cad4b05be758cd3de
Rahlien <rogerrabbit122@gmail.com>
2022-08-10 23:25:49 -0400

    finished todos, onto router

--- models/express-models/todos.js
+++ models/express-models/todos.js
@@ -1,4 +1,5 @@
-let tasks = {}; //
+let tasks = {};
+//
 
 /*
   tasks (defined above) will be a place to store tasks by person;
@@ -13,26 +14,51 @@ let tasks = {}; //
 module.exports = {
   reset: function () {
     tasks = {}; // (this function is completed for you.)
+    
   },
 
   // ==== COMPLETE THE FOLLOWING (SEE `model.js` TEST SPEC) =====
   listPeople: function () {
     // returns an array of all people for whom tasks exist
+    let peopleArr = Object.keys(tasks)
+    
+    return peopleArr
   },
 
   add: function (name, task) {
     // saves a task for a given person
+    
+    if (!tasks[name]){
+      tasks[name] = []
+    }
+    
+    if(tasks[name]){
+      tasks[name].push(task)
+    }
+    
+    for (let i = 0; i < tasks[name].length; i++){
+       if (!tasks[name][i].complete){
+        tasks[name][i].complete = false
+       }
+    }
+    
+  
+    console.log(tasks)
   },
 
   list: function (name) {
     // returns tasks for specified person
+    
+    return tasks[name]
   },
 
   complete: function (name, idx) {
     // marks a task complete
+    tasks[name][idx].complete = true
   },
 
   remove: function (name, idx) {
     // removes a tasks
+    tasks[name].splice(idx, 1)
   },
 };
--- routes/index.js
+++ routes/index.js
@@ -4,3 +4,13 @@ const todos = require('../models/express-models/todos');
 module.exports = router;
 
 // write your routes here. Feel free to split into multiple files if you like.
+
+router.get('/users', (req, res, next) => {
+    try{
+        res.send(res.body.listPeople)
+        
+    } 
+    catch (ex) {
+        next(ex)
+    }
+})
